557d4e829f575b2a5f1b3e5dd7b2674987ea16e2
Pedro Pereira <devpereirapedro@gmail.com>
2025-05-26 22:33:34 -0300

    fix: db connection

--- packages/data-access/src/db/client.ts
+++ packages/data-access/src/db/client.ts
@@ -1,12 +1,15 @@
+import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const db = drizzle({
   connection: {
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: Number(DB_PORT),
   },
 });
 
