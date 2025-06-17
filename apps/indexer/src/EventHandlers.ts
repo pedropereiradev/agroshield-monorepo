0e71f21c7b6b2341a33ed2f795500e7d6a455fb3
Pedro Pereira <devpereirapedro@gmail.com>
2025-06-17 01:25:35 -0300

    fix: change id

--- apps/indexer/src/EventHandlers.ts
+++ apps/indexer/src/EventHandlers.ts
@@ -1,24 +1,23 @@
 /*
  * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
  */
-import {
-  InsuranceManager,
-  type InsuranceManager_RegisterPolicyEvent,
-} from 'generated';
+import { InsuranceManager } from 'generated';
 
 InsuranceManager.RegisterPolicyEvent.handler(async ({ event, context }) => {
-  const entity: InsuranceManager_RegisterPolicyEvent = {
-    id: `${event.chainId}_${event.block.height}_${event.logIndex}`,
-    // policyId: event.params.policy_id.bits,
-    // owner: event.params.owner.payload.bits,
-    // insuredValue: event.params.insured_value.toString(),
-    // premium: event.params.premium.toString(),
-    // startDate: event.params.start_date.toString(),
-    // endDate: event.params.end_date.toString(),
-    // policyType: event.params.policy_type.case,
-    // status: event.params.status.case,
-    // timestamp: String(event.params.timestamp),
-  };
+  // const entity: InsuranceManager_RegisterPolicyEvent = {
+  //   id: `${event.chainId}_${event.block.height}_${event.logIndex}`,
+  //   // policyId: event.params.policy_id.bits,
+  //   // owner: event.params.owner.payload.bits,
+  //   // insuredValue: event.params.insured_value.toString(),
+  //   // premium: event.params.premium.toString(),
+  //   // startDate: event.params.start_date.toString(),
+  //   // endDate: event.params.end_date.toString(),
+  //   // policyType: event.params.policy_type.case,
+  //   // status: event.params.status.case,
+  //   // timestamp: String(event.params.timestamp),
+  // };
 
-  context.InsuranceManager_RegisterPolicyEvent.set(entity);
+  context.InsuranceManager_RegisterPolicyEvent.set({
+    id: `${event.chainId}-${event.params.policy_id.bits}`,
+  });
 });
