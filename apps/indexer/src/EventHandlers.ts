38a05332a9870c128627728be82558948c69f5b4
Pedro Pereira <devpereirapedro@gmail.com>
2025-06-17 22:49:50 -0300

    chore: add debug logs

--- apps/indexer/src/EventHandlers.ts
+++ apps/indexer/src/EventHandlers.ts
@@ -4,29 +4,20 @@
 import { InsuranceManager } from 'generated';
 
 InsuranceManager.RegisterPolicyEvent.handler(async ({ event, context }) => {
-  // const entity: InsuranceManager_RegisterPolicyEvent = {
-  //   id: `${event.chainId}_${event.block.height}_${event.logIndex}`,
-  //   // policyId: event.params.policy_id.bits,
-  //   // owner: event.params.owner.payload.bits,
-  //   // insuredValue: event.params.insured_value.toString(),
-  //   // premium: event.params.premium.toString(),
-  //   // startDate: event.params.start_date.toString(),
-  //   // endDate: event.params.end_date.toString(),
-  //   // policyType: event.params.policy_type.case,
-  //   // status: event.params.status.case,
-  //   // timestamp: String(event.params.timestamp),
-  // };
+  context.log.info(`ID: ${event.block.id}`);
+  context.log.info(`Policy ID: ${event.params.policy_id.bits}`);
+  context.log.info(`Timestamp: ${event.params.timestamp.toString()}`);
 
   context.InsuranceManager_RegisterPolicyEvent.set({
     id: `${event.block.id}-${event.params.timestamp.toString()}`,
-    policyId: event.params.policy_id.bits,
-    owner: event.params.owner.payload.bits,
-    insuredValue: event.params.insured_value.toString(),
-    premium: event.params.premium.toString(),
-    startDate: event.params.start_date.toString(),
-    endDate: event.params.end_date.toString(),
-    policyType: event.params.policy_type.case,
-    status: event.params.status.case,
-    timestamp: String(event.params.timestamp),
+    // policyId: event.params.policy_id.bits,
+    // owner: event.params.owner.payload.bits,
+    // insuredValue: event.params.insured_value.toString(),
+    // premium: event.params.premium.toString(),
+    // startDate: event.params.start_date.toString(),
+    // endDate: event.params.end_date.toString(),
+    // policyType: event.params.policy_type.case,
+    // status: event.params.status.case,
+    // timestamp: String(event.params.timestamp),
   });
 });
