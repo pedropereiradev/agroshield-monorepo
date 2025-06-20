7c4bcdca2fa08f1194b38a08097e46b630492e42
Pedro Pereira <devpereirapedro@gmail.com>
2025-06-20 14:40:05 -0300

    feat: remove sections that will not be used for now

--- apps/landing-page/app/page.tsx
+++ apps/landing-page/app/page.tsx
@@ -1,14 +1,11 @@
-"use client";
-
-import React from 'react';
+import BenefitsSection from '../components/BenefitsSection';
+import CTASection from '../components/CTASection';
+import FAQSection from '../components/FAQSection';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
-import ProblemSection from '../components/ProblemSection';
-import BenefitsSection from '../components/BenefitsSection';
 import HowItWorksSection from '../components/HowItWorksSection';
-import CoverageSection from '../components/CoverageSection';
-import CTASection from '../components/CTASection';
-import Footer from '../components/Footer';
+import ProblemSection from '../components/ProblemSection';
 
 export default function AgroShieldLanding() {
   return (
@@ -18,8 +15,9 @@ export default function AgroShieldLanding() {
       <ProblemSection />
       <BenefitsSection />
       <HowItWorksSection />
-      <CoverageSection />
-           <CTASection />
+      {/* <TestimonialsSection /> */}
+      <FAQSection />
+      <CTASection />
       <Footer />
     </div>
   );
