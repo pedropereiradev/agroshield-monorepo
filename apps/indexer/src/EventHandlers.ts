0bc45a4102dba9c500d43cfabbcb55f097ce9742
Pedro Pereira <devpereirapedro@gmail.com>
2025-06-24 00:20:10 -0300

    feat: add new events to indexer and modify schemas

--- apps/indexer/src/EventHandlers.ts
+++ apps/indexer/src/EventHandlers.ts
@@ -4,11 +4,7 @@
 import { InsuranceManager } from 'generated';
 
 InsuranceManager.RegisterPolicyEvent.handler(async ({ event, context }) => {
-  context.log.info(`ID: ${event.block.id}`);
-  context.log.info(`Policy ID: ${event.params.policy_id.bits}`);
-  context.log.info(`Timestamp: ${event.params.timestamp.toString()}`);
-
-  context.InsuranceManager_RegisterPolicyEvent.set({
+  context.Policies.set({
     id: `${event.chainId}_${event.block.height}_${event.logIndex}`,
     policyId: event.params.policy_id.bits,
     owner: event.params.owner.payload.bits,
@@ -20,4 +16,80 @@ InsuranceManager.RegisterPolicyEvent.handler(async ({ event, context }) => {
     status: event.params.status.case,
     timestamp: String(event.params.timestamp),
   });
+});
+
+InsuranceManager.RequestClaimEvent.handler(async ({ event, context }) => {
+  const policy = await context.Policies.get(
+    `${event.chainId}_${event.block.height}_${event.logIndex}`
+  );
+
+  if (policy) {
+    context.Policies.set({
+      ...policy,
+      status: event.params.new_status.case,
+    });
+
+    context.Claims.set({
+      id: `${event.chainId}_${event.block.height}_${event.logIndex}`,
+      policy_id: policy.id,
+      timestamp: event.params.timestamp.toString(),
+      oldStatus: event.params.old_status.case,
+      newStatus: event.params.new_status.case,
+    });
+  }
+});
+
+InsuranceManager.ApproveClaimEvent.handler(async ({ event, context }) => {
+  const policy = await context.Policies.get(
+    `${event.chainId}_${event.block.height}_${event.logIndex}`
+  );
+
+  if (policy) {
+    context.Policies.set({
+      ...policy,
+      status: event.params.new_status.case,
+    });
+
+    context.Claims.set({
+      id: `${event.chainId}_${event.block.height}_${event.logIndex}`,
+      policy_id: policy.id,
+      timestamp: event.params.timestamp.toString(),
+      oldStatus: event.params.old_status.case,
+      newStatus: event.params.new_status.case,
+    });
+  }
+});
+
+InsuranceManager.RejectClaimEvent.handler(async ({ event, context }) => {
+  const policy = await context.Policies.get(
+    `${event.chainId}_${event.block.height}_${event.logIndex}`
+  );
+
+  if (policy) {
+    context.Policies.set({
+      ...policy,
+      status: event.params.new_status.case,
+    });
+
+    context.Claims.set({
+      id: `${event.chainId}_${event.block.height}_${event.logIndex}`,
+      policy_id: policy.id,
+      timestamp: event.params.timestamp.toString(),
+      oldStatus: event.params.old_status.case,
+      newStatus: event.params.new_status.case,
+    });
+  }
+});
+
+InsuranceManager.ExpirePolicyEvent.handler(async ({ event, context }) => {
+  const policy = await context.Policies.get(
+    `${event.chainId}_${event.block.height}_${event.logIndex}`
+  );
+
+  if (policy) {
+    context.Policies.set({
+      ...policy,
+      status: event.params.new_status.case,
+    });
+  }
 });
