8a60cae0ced1a7c93b428456aee37527e346e918
Pedro Pereira <devpereirapedro@gmail.com>
2025-06-29 14:16:34 -0300

    fix: remove hasura secret

--- packages/graphql-client/src/config.ts
+++ packages/graphql-client/src/config.ts
@@ -8,14 +8,8 @@ export function createGraphQLConfig(): GraphQLConfig {
     //@ts-ignore
     import.meta.env.VITE_INDEXER_GRAPHQL_ENDPOINT ||
     'http://localhost:8080/v1/graphql';
-  const adminSecret =
-    //@ts-ignore
-    import.meta.env.VITE_INDEXER_HASURA_ADMIN_SECRET || 'testing';
 
   return {
     endpoint,
-    headers: {
-      'x-hasura-admin-secret': adminSecret,
-    },
   };
 }
