11ab7be2533d984414e38fa15487e24e95329ad1
Pedro Pereira <devpereirapedro@gmail.com>
2025-11-12 16:46:04 -0300

    feat: Add current weather client to Open-Meteo API
    Export new OpenMeteoCurrentWeatherClient and geocoding module from
    data-access package

--- packages/data-access/src/index.ts
+++ packages/data-access/src/index.ts
@@ -1,3 +1,4 @@
 export * from './weather';
 export * from './entities';
 export * from './location';
+export * from './geocoding';
--- packages/data-access/src/weather/open-meteo/index.ts
+++ packages/data-access/src/weather/open-meteo/index.ts
@@ -1,4 +1,10 @@
+import { OpenMeteoCurrentWeatherClient } from './current-weather-client';
 import { OpenMeteoForecastClient } from './forecast-client';
 import { OpenMeteoHistoricalClient } from './historical-client';
 
-export { OpenMeteoHistoricalClient, OpenMeteoForecastClient };
+export {
+  OpenMeteoHistoricalClient,
+  OpenMeteoForecastClient,
+  OpenMeteoCurrentWeatherClient,
+};
+export * from './current-weather-client';
